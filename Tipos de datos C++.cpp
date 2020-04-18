bfd14374065955085a80ea0a0a7f4ac786d566ad
Miguel Lopez <miguellopezv1122@gmail.com>
2020-04-18 16:54:33 -0500

    Tipos Variables

---
+++ Tipos de datos C++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int entero = 15;
+	float flotante = 10.45;
+	double mayor = 16.3456;
+	char letra = 'a';
+	
+	cout<<entero<<endl;
+	cout<<""<<endl;
+	cout<<flotante<<endl;
+	cout<<""<<endl;
+	cout<<mayor<<endl;
+	cout<<""<<endl;
+	cout<<letra;
+	
+	return 0;
+}
